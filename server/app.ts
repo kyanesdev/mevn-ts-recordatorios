f4a68784d4f69293403016500902486b01a92d01
kyanesdev <kyanesdev@gmail.com>
2022-08-18 11:25:34 -0300

    first commit

---
+++ server/app.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import morgan from 'morgan';
+import cors from 'cors';
+import taskRoutes from './routes';
+
+const app = express()
+
+//Middlewares
+app.use(cors());
+app.use(morgan('dev'))
+app.use(express.json())
+
+app.use('/api' , taskRoutes)
+
+export default app;
---
+++ server/routes/index.ts
@@ -0,0 +1,54 @@
+import { Router } from 'express';
+import Task from '../models/Tasks';
+
+const router = Router();
+
+router.get('/tasks', async(req, res) => {
+    const tasks = await Task.find()
+    res.send(tasks);
+});
+
+router.get('/tasks/:id', async(req, res) => {
+   try {
+    const task = await Task.findById(req.params.id);
+    res.send(task);
+
+    if(!task) {
+        res.status(404).send("Tarea no encontrada");
+    }
+
+   } catch (error) {
+         res.status(500).send(error);
+   }
+});
+
+router.post('/tasks', async (req, res) => {
+    const {title , description} = req.body;
+
+    const task = new Task({title, description})
+
+    await task.save();
+
+    res.json(task);
+});
+
+router.delete('/tasks/:id', async(req, res) => {
+
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        res.send('Tarea encontrada y eliminada: '+ task?.title);
+
+    if(!task) {
+        res.status(404).send("Tarea no encontrada");
+    }
+    } catch (error) {
+        res.status(500).send(error);
+        
+    }
+});
+
+router.put('/tasks/:id', (req, res) => {
+    res.send('Actualizando una tarea');
+});
+
+export default router;
